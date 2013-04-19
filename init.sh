fe7f7fbc0441b189e864f0b534f3da9d28c0bd0f
acidrain42 <acidrain1@gmail.com>
2013-04-19 11:36:44 -0400

    Added init script

---
+++ init.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+ln -s .dotfiles/zshrc .zshrc
+ln -s .dotfiles/zsh_aliases .zsh_aliases
+ln -s .dotfiles/zsh_functions .zsh_functions
+ln -s .dotfiles/vim .vim
+ln -s .dotfiles/vimrc .vimrc
+ln -s .dotfiles/xinitrc .xinitrc
+ln -s .dotfiles/Xmodmap .Xmodmap
+ln -s .dotfiles/SciTEUser.properties .SciTEUser.properties
+ln -s .dotfiles/gitconfig .gitconfig
+ln -s .dotfiles/templates .templates
+ln -s .dotfiles/tmux.conf .tmux.conf
