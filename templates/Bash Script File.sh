a39649dc01604d1644bd7743a1f28637b9f6b439
Mathieu Lemay <acidrain1@gmail.com>
2018-07-16 22:01:14 -0400

    Bash Template: Update logging

--- templates/Bash Script File.sh
+++ templates/Bash Script File.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -eu
 
-info()    { echo -e "\e[32m[INFO]\e[39m    $*" ; }
-warning() { echo -e "\e[33m[WARNING]\e[39m $*" ; }
-error()   { echo -e "\e[31m[ERROR]\e[39m   $*" 1>&2 ; }
-fatal()   { echo -e "\e[35m[FATAL]\e[39m   $*" 1>&2 ; exit 1 ; }
+info()    { printf "\\e[32m[INFO]\\e[0m    %s\\n" "$*" ; }
+warning() { printf "\\e[33m[WARNING]\\e[0m %s\\n" "$*" ; }
+error()   { printf "\\e[31m[ERROR]\\e[0m   %s\\n" "$*" >&2 ; }
+fatal()   { printf "\\e[35m[FATAL]\\e[0m   %s\\n" "$*" >&2 ; exit 1 ; }
