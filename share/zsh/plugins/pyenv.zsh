07eddd8626c2221bf832f9af71f7a565becb23dd
Mathieu Lemay <acidrain1@gmail.com>
2019-10-18 10:24:07 -0400

    zsh: add pyenv plugin

---
+++ share/zsh/plugins/pyenv.zsh
@@ -0,0 +1,25 @@
+type pyenv &> /dev/null || return
+
+export PATH="/home/mathieu/.pyenv/shims:${PATH}"
+export PYENV_SHELL=zsh
+
+pyenv() {
+    local command
+    command="${1:-}"
+    if [ "$#" -gt 0 ]; then
+        shift
+    fi
+
+    case "$command" in
+    rehash|shell)
+        eval "$(pyenv "sh-$command" "$@")"
+        ;;
+    install|uninstall)
+        command pyenv "$command" "$@"
+        pyenv rehash
+        ;;
+    *)
+        command pyenv "$command" "$@"
+        ;;
+    esac
+}
