0cae6c5e91ec40e1a5071a18b4b9325557b48f07
Mathieu Lemay <acidrain1@gmail.com>
2019-10-28 22:30:51 -0400

    pyenv: Fix VIRTUALENVWRAPPER_PYTHON

--- share/zsh/plugins/pyenv.zsh
+++ share/zsh/plugins/pyenv.zsh
@@ -3,6 +3,9 @@ type pyenv &> /dev/null || return
 export PATH="/home/mathieu/.pyenv/shims:${PATH}"
 export PYENV_SHELL=zsh
 
+# PyEnv doesn't play well with virtualenvwrapper
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
+
 pyenv() {
     local command
     command="${1:-}"
