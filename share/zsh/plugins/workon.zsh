48d288eff0b5c35445694f7316b65c583cab72df
Mathieu Lemay <acidrain1@gmail.com>
2020-05-13 14:37:57 -0400

    workon.zsh: add plugin

---
+++ share/zsh/plugins/workon.zsh
@@ -0,0 +1,28 @@
+function w() {
+    local srcdir="${SRC_DIR:-${HOME}/src}"
+    local venvdir="${WORKON_HOME:-${HOME}/.virtualenvs}"
+    if [[ $# != 1 ]]; then
+        echo "Usage: $0 env"
+        return 1
+    fi
+
+    local envname="$1"
+
+    if [[ -d "${venvdir}/$envname" ]]; then
+        workon "$envname"
+        return 0
+    fi
+
+    local dir
+
+    if hash -d | cut -d '=' -f1 | grep -q "$envname"; then
+        cd ~"${envname}"
+    elif [[ -d "${srcdir}/$envname" ]]; then
+        cd "${srcdir}/${envname}"
+    else
+        echo "Invalid environment: ${envname}" 1>&2
+        return 1
+    fi
+
+    [[ -e "pyproject.toml" ]] && poetry shell
+}
