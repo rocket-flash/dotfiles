93c949e64e282b2efa51ce00acf98cac88a3ce08
Mathieu Lemay <acidrain1@gmail.com>
2020-09-29 18:04:07 -0400

    aws.zsh: Add aws-ecr-login and utils

--- share/zsh/plugins/aws.zsh
+++ share/zsh/plugins/aws.zsh
@@ -1,5 +1,17 @@
 AWS_PROFILE_CACHE_FILE="$HOME/.cache/zsh/aws-profile"
 
+function aws-get-account-id() {
+    aws sts get-caller-identity | jq -r '.Account'
+}
+
+function aws-get-current-region() {
+    if [ -n "${AWS_PROFILE}" ]; then
+        aws configure get --profile "${AWS_PROFILE}" region
+    else
+        aws configure get region
+    fi
+}
+
 function aws-list-profiles() {
     grep -oE '\[profile [a-zA-Z0-9.-]+\]' "${AWS_CONFIG_FILE}" | sed 's/[][]//g' | awk -F ' ' '{print $2}' | sed 's/^/  /'
 }
@@ -34,7 +46,7 @@ function aws-assume-role {
         return
     fi
 
-    account="$(aws sts get-caller-identity | jq -r '.Account')"
+    account="$(aws-get-account-id)"
     arn="arn:aws:iam::${account}:role/CrossAccountAdminRole"
 
     credentials="$(aws sts assume-role --role-arn "$arn" --role-session-name "$AWS_PROFILE")"
@@ -47,6 +59,13 @@ function aws-assume-role {
     export AWS_SECRET_ACCESS_KEY="$(echo "$credentials" | jq -r '.Credentials.SecretAccessKey')"
     export AWS_SESSION_TOKEN="$(echo "$credentials" | jq -r '.Credentials.SessionToken')"
     set +x
+}
+
+function aws-ecr-login() {
+    aws ecr get-login-password | docker login \
+        --username AWS --password-stdin \
+        "$(aws-get-account-id).dkr.ecr.$(aws-get-current-region).amazonaws.com"
+
 }
 
 [[ -d "$(dirname "${AWS_PROFILE_CACHE_FILE}")" ]] || mkdir -p "$(dirname "${AWS_PROFILE_CACHE_FILE}")"
