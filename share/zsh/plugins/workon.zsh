4c7a395393d47063ed35ff641cbed7930947c144
Mathieu Lemay <acidrain1@gmail.com>
2020-11-16 13:57:25 -0500

    workon.zsh: cd to first argument if possible

--- share/zsh/plugins/workon.zsh
+++ share/zsh/plugins/workon.zsh
@@ -1,15 +1,31 @@
-function w() {
-    local srcdir="${SRC_DIR:-${HOME}/src}"
-    local prj="$1"
-
-    if [[ -z "${prj}" ]]; then
-        prj=$(find ~/src -maxdepth 1 -mindepth 1 -print0 | xargs -0 -n1 basename | fzf)
-    fi
+function _get_workon_dir() {
+    local srcdir=$1
+    local prj=$2
 
     if hash -d | cut -d '=' -f1 | grep -q "${prj}"; then
-        cd ~"${prj}"
+        echo ~"${prj}"
     elif [[ -d "${srcdir}/${prj}" ]]; then
-        cd "${srcdir}/${prj}"
+        echo "${srcdir}/${prj}"
+    fi
+}
+
+function w() {
+    local srcdir="${SRC_DIR:-${HOME}/src}"
+
+    if [[ -n "${1:-}" ]]; then
+        local workondir="$(_get_workon_dir "${srcdir}" "$1")"
+        if [[ -n "${workondir}" ]]; then
+            cd "${workondir}"
+            return 0
+        fi
+    fi
+
+    prj=$(find ~/src -maxdepth 1 -mindepth 1 -print0 | xargs -0 -n1 basename | fzf -q "${@:-}")
+
+    local workondir="$(_get_workon_dir "${srcdir}" "${prj}")"
+
+    if [[ -n "${workondir}" ]]; then
+        cd "${workondir}"
     else
         echo "Invalid environment: ${prj}" 1>&2
         return 1
