e96222c36535ee2c5b8b44d61ee0572c80153186
Mathieu Lemay <acidrain1@gmail.com>
2021-05-28 12:59:08 -0400

    Move arch specific aliases to arch zsh plugin

---
+++ share/zsh/plugins/arch.zsh
@@ -0,0 +1,77 @@
+if type paru &>/dev/null; then
+    pm() {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            echo "Can't run paru in venv"
+            return 1
+        fi
+
+        paru "$@"
+    }
+
+    up() {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            echo "Can't run paru in venv"
+            return 1
+        fi
+
+        paru -Syu "$@"
+    }
+
+    pmi() {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            echo "Can't run paru in venv"
+            return 1
+        fi
+
+        paru -Slq | fzf --multi --preview 'paru -Si {1}' -q "${@:-}" | xargs -ro paru -S
+    }
+
+    pmr() {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            echo "Can't run paru in venv"
+            return 1
+        fi
+
+        paru -Qq | fzf --multi --preview 'paru -Si {1}' -q "${@:-}" | xargs -ro paru -Rns
+    }
+
+    compdef pm='paru'
+elif type yay &>/dev/null; then
+    pm() {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            echo "Can't run yay in venv"
+            return 1
+        fi
+
+        yay "$@"
+    }
+
+    up() {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            echo "Can't run yay in venv"
+            return 1
+        fi
+
+        yay -Syu "$@"
+    }
+
+    pmi() {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            echo "Can't run yay in venv"
+            return 1
+        fi
+
+        yay -Slq | fzf --multi --preview 'yay -Si {1}' -q "${@:-}" | xargs -ro yay -S
+    }
+
+    pmr() {
+        if [ -n "${VIRTUAL_ENV}" ]; then
+            echo "Can't run yay in venv"
+            return 1
+        fi
+
+        yay -Qq | fzf --multi --preview 'yay -Si {1}' -q "${@:-}" | xargs -ro yay -Rns
+    }
+
+    compdef pm='yay'
+fi
