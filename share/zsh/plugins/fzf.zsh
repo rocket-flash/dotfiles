921dcfb9f0d7b85d8ba6c49f1d2ae34e2a38ce25
Mathieu Lemay <acidrain1@gmail.com>
2021-05-28 13:01:00 -0400

    fzf: Use tmux if possible, add previews and add command for ALT_C

--- share/zsh/plugins/fzf.zsh
+++ share/zsh/plugins/fzf.zsh
@@ -3,14 +3,21 @@
 . /usr/share/fzf/key-bindings.zsh
 . /usr/share/fzf/completion.zsh
 
+preview_window_opts="--preview-window hidden --bind '?:toggle-preview'"
+
 # Show prompt on top
-export FZF_DEFAULT_OPTS='--reverse -1'
+export FZF_DEFAULT_OPTS="--reverse -1"
 # --files: List files that would be searched but do not search
 # --follow: Follow symlinks
-export FZF_DEFAULT_COMMAND='rg --files --follow 2>/dev/null'
-export FZF_COMPLETION_TRIGGER=';;'
+export FZF_DEFAULT_COMMAND="rg --files --follow 2>/dev/null"
+export FZF_COMPLETION_TRIGGER=";;"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_OPTS="--tiebreak=end"
+export FZF_CTRL_T_OPTS="--tiebreak=end --preview '(bat --color=always {} || cat {}) 2>/dev/null | head -200' ${=preview_window_opts}"
+export FZF_ALT_C_COMMAND="fd --type=d"
+export FZF_ALT_C_OPTS="--tiebreak=end --preview 'tree -C {} | head -200' ${=preview_window_opts}"
+export FZF_TMUX_OPTS="-p"
+
+unset preview_window_opts
 
 # Use ripgrep instead of the default find command for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
