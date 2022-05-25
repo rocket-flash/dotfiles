d1c712c2c640af2b49af3e74ce02cf813acfd553
Mathieu Lemay <acidrain1@gmail.com>
2022-05-25 13:36:48 -0400

    k8s: add ksel command and kx/kn aliases

--- share/zsh/plugins/k8s.zsh
+++ share/zsh/plugins/k8s.zsh
@@ -17,3 +17,13 @@ if command -v k9s &>/dev/null; then
         command k9s
     }
 fi
+
+if command -v kubectx &>/dev/null; then
+    functions ksel() {
+        kubectx "${1:-}" || return 1
+        kubens "${2:-}" || return 1
+    }
+
+    alias kx=kubectx
+    alias kn=kubens
+fi
