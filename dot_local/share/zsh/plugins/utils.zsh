6a8b6864ad1c8012d06b96b24df99508f2beecfe
Mathieu Lemay <mathieu@boostsecurity.io>
2022-06-22 13:10:19 -0400

    utils.zsh: Allow parameter to `dotenv load` command

--- dot_local/share/zsh/plugins/utils.zsh
+++ dot_local/share/zsh/plugins/utils.zsh
@@ -32,7 +32,8 @@ if command -v dotenv &> /dev/null; then
     function dotenv() {
         case "$1" in
             load)
-                . <(dotenv list | sed -E 's/^([^=]+)=(.*)/export \1="\2"/')
+                shift 1
+                . <(dotenv "$@" list | sed -E 's/^([^=]+)=(.*)/export \1="\2"/')
                 ;;
             *)
                 command dotenv "$@"
