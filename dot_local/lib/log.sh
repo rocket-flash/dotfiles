8b88b65f383b02c12065d2d6ca3ec15e022e5071
Mathieu Lemay <mathieu@boostsecurity.io>
2022-10-05 09:10:18 -0400

    log.sh: Add prompt()

--- dot_local/lib/log.sh
+++ dot_local/lib/log.sh
@@ -2,6 +2,7 @@ info()    { printf "\\e[32m[INFO]\\e[0m    %s\\n" "$*" >&2 ; }
 warning() { printf "\\e[33m[WARNING]\\e[0m %s\\n" "$*" >&2 ; }
 error()   { printf "\\e[31m[ERROR]\\e[0m   %s\\n" "$*" >&2 ; }
 fatal()   { printf "\\e[35m[FATAL]\\e[0m   %s\\n" "$*" >&2 ; exit 1 ; }
+prompt()  { printf "\\e[36m[PROMPT]\\e[0m  %s\\n" "$*" >&2 ; }
 
 # TODO: Make this POSIX compatible
 if [[ -n "${ZSH_VERSION:-}" ]]; then
